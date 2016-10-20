2b8752cc2fe92c095126e36d3a397b166817df75
Vlad Orzhekhovskiy <vlad@uralian.com>
2016-10-20 11:44:14 -0400

    Implement Settings and basic Kafka helper functions

---
+++ src/main/scala/org/dsa/iot/kafka10/Settings.scala
@@ -0,0 +1,18 @@
+package org.dsa.iot.kafka10
+
+import com.typesafe.config.ConfigFactory
+
+/**
+ * Kafka DSLink settings.
+ */
+object Settings {
+  private lazy val config = ConfigFactory.load
+
+  val DEFAULT_HOST = config.getString("defaultHost")
+  val DEFAULT_PORT = config.getInt("defaultPort")
+  val DEFAULT_BROKER_URL = DEFAULT_HOST + ":" + DEFAULT_PORT
+
+  val AUX_GROUP_ID = config.getString("auxGroupId")
+
+  val ACTION_RETRIES = config.getInt("actionRetries")
+}
